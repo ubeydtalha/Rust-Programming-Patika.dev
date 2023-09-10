93dc473df9829da2f6bf9bfdbb5fde8e1c14304e
ubeyd <ubeydtalhaalkan@gmail.com>
2023-09-10 10:13:38 +0200

    iter

---
+++ p2/iterators/target/debug/deps/iterators.d
@@ -0,0 +1,5 @@
+C:\Users\utabj\Rust\p2\iterators\target\debug\deps\iterators.exe: src\main.rs
+
+C:\Users\utabj\Rust\p2\iterators\target\debug\deps\iterators.d: src\main.rs
+
+src\main.rs:
---
+++ p2/iterators/target/debug/iterators.d
@@ -0,0 +1,1 @@
+C:\Users\utabj\Rust\p2\iterators\target\debug\iterators.exe: C:\Users\utabj\Rust\p2\iterators\src\main.rs
